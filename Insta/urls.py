2f0b00016624641a45b631ebf0ebd2513faa2717
dingchang <yang.din@husky.neu.edu>
2020-03-09 07:07:27 -0400

    CRUD experiment

--- Insta/urls.py
+++ Insta/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', HelloWorld.as_view(), name='helloworld'),
+    path('posts/', PostView.as_view(), name='posts'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
+    path('posts/edit/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
 ]
